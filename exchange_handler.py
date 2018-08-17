5f8ddb826a6650d5a1d2c37148864c32f02bb9f1
johnfranciscosebastian <37100741+johnfranciscosebastian@users.noreply.github.com>
2018-08-17 12:55:16 +0800

    New function
    Claim Position to close positions

--- exchange_handler.py
+++ exchange_handler.py
@@ -5,11 +5,11 @@ def getTicker(exchange, coin_pair):
                 ticker = bitfinex_v1.getTicker(coin_pair)
         return ticker['bid']
 
-def getWalletBalances(exchange, type, currency):
+def getWalletBalances(exchange, wallet_type, currency):
         if exchange == 'bitfinexv1':
                 wallets = bitfinex_v1.BitfinexClient().getWalletBalances()
         for wallet in wallets:
-                if wallet['type'] == type and wallet['currency'] == currency:
+                if wallet['type'] == wallet_type and wallet['currency'] == currency:
                         wallet_balance = wallet['available']
         return wallet_balance
 
@@ -18,18 +18,23 @@ def getActivePositions(exchange):
                 active_positions = bitfinex_v1.BitfinexClient().getActivePositions()
         return active_positions
 
-def newOrder(exchange, symbol, amount, price, side, type):
+def newOrder(exchange, symbol, amount, price, side, order_type):
         if exchange == 'bitfinexv1':
                 #price is arbitrary for market orders
-                order = bitfinex_v1.BitfinexClient().newOrder(symbol, amount, price, side, type)
+                order = bitfinex_v1.BitfinexClient().newOrder(symbol, amount, price, side, order_type)
         return order
 
-def getOrderStatus(exchange, id):
+def getOrderStatus(exchange, trade_id):
         if exchange == 'bitfinexv1':
-                status = bitfinex_v1.BitfinexClient().getOrderStatus(id)
+                status = bitfinex_v1.BitfinexClient().getOrderStatus(trade_id)
         return status
 
 def transferWallet(exchange, from_type, to_type, amount, currency):
         if exchange == 'bitfinexv1':
                 transfer = bitfinex_v1.BitfinexClient().transferWallet(from_type, to_type, amount, currency)
         return transfer
+
+def claimPosition(exchange, xchangeID, amount):
+        if exchange == 'bitfinexv1':
+                claim = bitfinex_v1.BitfinexClient().claimPosition(xchangeID, amount)
+        return claim
